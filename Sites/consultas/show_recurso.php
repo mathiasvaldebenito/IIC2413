08ee2d0765a9f0d53e0f0421aa837830e6004c66
roberto009 <rsnegrin@uc.cl>
2019-10-24 10:56:41 -0300

    recursos

---
+++ Sites/consultas/show_recurso.php
@@ -0,0 +1,47 @@
+<?php require("./../config.php");?>
+<?php include('./../templates/header.php');   ?>
+
+<?php
+    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+  $recurso_numbr = $_GET["number"];
+  $query = "SELECT *
+            FROM recurso
+            WHERE numero='$recurso_numbr';";
+  $result = $db52 -> prepare($query);
+  $result -> execute();
+  $results = $result -> fetchAll();
+?>
+<br>
+
+<div class=container>
+  <div class="row justify-content-center">
+    <div class="d-inline-flex" style="overflow: auto; max-height: 500px;">
+      <table class="table table-hover table-md w-auto">
+        <thead class="thead-dark" style="position: sticky; top: 0;">
+          <tr>
+            <th>Numero Recurso</th>
+            <th>Comuna Tramitacion</th>
+            <th>Causa Contaminacion</th>
+            <th>Area Influencia</th>
+            <th>Descripcion</th>
+            <th>Fecha Apertura</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <?php
+        foreach ($results as $row) {
+            echo "<tr> <td>$row[0]</td>
+                       <td>$row[1]</td>
+                       <td>$row[2]</td>
+                       <td>$row[3]</td>
+                       <td>$row[4]</td>
+                       <td>$row[5]</td>
+                       <td>$row[6]</td>
+                       </tr>";
+        }
+        ?>
+      </table>
+    </div>
+  </div>
+</div>
