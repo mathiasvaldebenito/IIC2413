d4656d8dd8f08f85ab4e87a8773e5d55f6258366
mathiasvaldebenito <mlvaldebenito@uc.cl>
2019-12-02 01:02:17 -0300

    Entrega4

---
+++ Entrega3/README.md
@@ -0,0 +1,20 @@
+* En la aplicación solo se pueden registrar ongs y socios que
+  existían previamente en la base de datos.
+  * Credenciales:
+    * Credenciales para ingresar como socio:
+        * nombre: Henedina
+        * apellido: Cantolla
+        * contraseña: 123456
+
+    * Credenciales para ingresar como ONG:
+        * usuario: Mertz Group
+        * contraseña: 123456
+
+* Al ingresar como socio o ong se desbloquean sus respectivas facultades.
+  * Para ver las movilizaciones de la ong se debe ingresar como ong y luego presionar en
+   'Mis movilizaciones' en la barra de navegación.
+  * Para crear proyectos se debe ingresar como socio y luego presionar en 'Crea un Proyecto!'
+* Se muestran los atributos de las ONG, a través de la barra de navegación se presiona en ONG
+  y luego se presiona en cualquier ong de las que se despliegan y se mostrarán sus recursos asociados y movilizaciones.
+  Una vez dentro se puede presionar en cualquier numero de los recursos y se muestran todos sus atributos.
+* Se puede acceder a los recursos desde todas las tablas que tengan la columna numero recurso.
---
+++ Entrega4/main.py
@@ -0,0 +1,64 @@
+﻿from flask import Flask, render_template, request, abort, json
+from pymongo import MongoClient
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+'''
+    Código extraído de AyudantiaAPI.
+'''
+
+MESSAGES_KEYS = ['content', 'metadata'] #not sure if this actually works
+
+#URL = "mongodb://grupo41:grupo41@gray.ing.puc.cl/grupo41"
+#client = MongoClient(URL)
+#db = client.get_database()
+client = MongoClient()
+db = client["entidades"]
+messages = db.documentos
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>HELLO API GRUPO 41-52</h1>"
+
+@app.route("/messages")
+def get_messages():
+    resultados = [m for m in messages.find({},{"_id":0})]
+    return json.jsonify(resultados)
+
+@app.route("/messages/<int:mid>")
+def get_message(mid):
+    message = list(messages.find({"mid":mid},{"_id":0}))
+    return json.jsonify(message)
+
+@app.route("/messages/project-search")
+def get_project_messages():
+    '''
+    Implement code to return all the messages sended and received
+    by the partners of the same project.
+    Example:  "/messages/project-search?name_project=xxxxx".
+    '''
+
+@app.route("/messages/content-search")
+def get_content_messages():
+    '''
+    Implement code to return all the messages with the wanted words or wanted
+    sentences or prohibited word in the content.
+    '''
+
+@app.route("/messages", methods=['POST'])
+def create_message():
+    '''
+    Implement code to create a new message in the db
+    '''
+
+@app.route("/messages/<int:mid>", methods=['DELETE'])
+def delete_messages(mid):
+    messages.delete_one({"mid":mid})
+    message = f'Mensaje con id={mid} ha sido eliminado'
+    return json.jsonify({'result': 'succes', 'message': message})
+
+if __name__ == "__main__":
+    app.run()
